e8aeb0cd49c3dc6b237e285011ab784f78d11860
Maryna-aya <66226281+Maryna-aya@users.noreply.github.com>
2022-11-08 22:04:03 +0100

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+function App() {
+  return (
+    <div className="App">
+      NEW APPP
+      <button>
+        NEW BUTTON
+      </button>
+    </div>
+  );
+}
+
+export default App;
